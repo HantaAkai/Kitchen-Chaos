fd135799701c09e5fc477e18edda8eae204441cd
HantaAkai <hantaakai@gmail.com>
2023-03-02 17:11:01 +0100

    Implemented IKitchenObjectParent interface.

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,29 +6,29 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] KitchenObjectSO kithcenObjectSO;
 
-    private ClearCounter clearCounter;
+    private IKitchenObjectParent kitchenObjectParent;
 
     public KitchenObjectSO GetKithcenObjectSO() {
         return kithcenObjectSO;
     }
 
-    public void SetClearCounter(ClearCounter clearCounter) {
-        if (this.clearCounter != null) { 
-            this.clearCounter.ClearKitchenObject();
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent) {
+        if (this.kitchenObjectParent != null) { 
+            this.kitchenObjectParent.ClearKitchenObject();
         }
-        this.clearCounter = clearCounter;
+        this.kitchenObjectParent = kitchenObjectParent;
 
-        if (clearCounter.HasKithcenObject()) {
-            Debug.LogError("This counter already has a kithcen object!");
+        if (kitchenObjectParent.HasKithcenObject()) {
+            Debug.LogError("This IKitchenObjectParent already has a KithcenObject!");
         }
 
-        clearCounter.SetKitchenObject(this);
+        kitchenObjectParent.SetKitchenObject(this);
 
-        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
         transform.localPosition = Vector3.zero; 
     }
 
-    public ClearCounter GetClearCounter() {
-        return clearCounter;
+    public IKitchenObjectParent GetKitchenObjectParent() {
+        return kitchenObjectParent;
     }
 }
