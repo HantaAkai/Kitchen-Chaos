bcf280e654f9f403511872e325628c7573261b14
HantaAkai <hantaakai@gmail.com>
2023-03-02 23:23:24 +0100

    Build put down / pick up functionality for clear counters.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -11,12 +11,22 @@ public class ClearCounter : BaseCounter
         if (!HasKithcenObject()) {
             //There is no KitchenObject here
 
-            if (player.HasKithcenObject()) { 
+            if (player.HasKithcenObject()) {
                 //Player has something
                 player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else { 
+                //Player has nothing
             }
-        } else { 
+        } else {
             // There is a KitchenObject here
+
+            if (player.HasKithcenObject()) {
+                //Player has something
+            } else {
+                //Player has nothing
+
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
         }        
     }
 
