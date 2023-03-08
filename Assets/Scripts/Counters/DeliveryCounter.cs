820e98e33d1be6b1ca856d99d6733dfd922e8a15
HantaAkai <hantaakai@gmail.com>
2023-03-08 20:00:51 +0100

    Build MovingVisual Shader Graph.

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter {
+
+    public override void Interact(Player player) {
+
+        if (player.HasKithcenObject()) {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                //Player has a plate
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    
+    }
+
+}
